90ad1392c9266dd5cff9f0005a2d39ecfec3534d
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-03 23:19:57 +0530

    Update 1.2-3.py

--- CLRS/1.2-3.py
+++ CLRS/1.2-3.py
@@ -1,8 +1,12 @@
-import math
-n = 1
-while 100*n*n > 2^n:
+n = 
+while 100*(n^2) > (2^n):
     n +=1
     print(n)
+    if n > 100:
+        print("Error Error Error")
+        break
 print("Min value that violates the inequality is n = ", n)
 
 
+# while 100 * n * n  > 2 ** n:
+    # this code is working 
