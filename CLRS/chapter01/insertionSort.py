ef8a2fb1f8da33d38d69abf832e1a28875e7ad29
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-10 09:54:42 +0530

    added pseudocode

--- CLRS/chapter01/insertionSort.py
+++ CLRS/chapter01/insertionSort.py
@@ -3,5 +3,16 @@
 
 
 # INSERTION SORT PSEUDOCODE
+# ---------------------------------------------------------------
 #
+#    [INSERTOIN SORT(A,N)
+#        [for i = 2 to n
+#            [key = A[i]
+#            [// Insert A{i} into the sorted subarrary A[1:i-1]
+#            [j = i - 1
+#            [while j > 0 and A[j] > key
+#                [A[j+1] = A[j]
+#                [j = j - 1
+#            [A[j+1] = key
 #
+# ----------------------------------------------------------------
