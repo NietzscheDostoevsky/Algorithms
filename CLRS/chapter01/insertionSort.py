2229dec469fe90fc78b9ed5ca3c034564f211faf
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-10 10:10:25 +0530

    function defition

--- CLRS/chapter01/insertionSort.py
+++ CLRS/chapter01/insertionSort.py
@@ -5,14 +5,27 @@
 # INSERTION SORT PSEUDOCODE
 # ---------------------------------------------------------------
 #
-#    [INSERTOIN SORT(A,N)
-#        [for i = 2 to n
-#            [key = A[i]
-#            [// Insert A{i} into the sorted subarrary A[1:i-1]
-#            [j = i - 1
-#            [while j > 0 and A[j] > key
-#                [A[j+1] = A[j]
-#                [j = j - 1
-#            [A[j+1] = key
+#    INSERTOIN SORT(A,N)
+#        for i = 2 to n
+#            key = A[i]
+#            // Insert A{i} into the sorted subarrary A[1:i-1]
+#            j = i - 1
+#            while j > 0 and A[j] > key
+#                A[j+1] = A[j]
+#                j = j - 1
+#            A[j+1] = key
 #
 # ----------------------------------------------------------------
+
+
+def insertionsort(a, n=None):
+    """Takes an array of size n, and returns it sorted using insertion sort
+
+    Args:
+        a (list): a list of comparable elements
+        n (integer): size of the array to be sorted
+        return : sorted array a
+    """
+
+    if n is None:
+        n = len(a)
