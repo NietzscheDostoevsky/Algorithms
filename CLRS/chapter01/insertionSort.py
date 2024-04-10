0da61475ce6fabd69ca676c55f3c18d6764823ee
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2024-04-10 11:30:23 +0530

    Update insertionSort.py

--- CLRS/chapter01/insertionSort.py
+++ CLRS/chapter01/insertionSort.py
@@ -24,21 +24,31 @@ def insertionsort(a, n=None):
     Args:
         a (list): a list of comparable elements
         n (integer): size of the array to be sorted
-        return : sorted array a
+    Returns:
+        list: sorted array a
     """
+    # Make a copy of the original list to preserve it
+    a_copy = a.copy()
 
     if n is None:
-        n = len(a)
+        n = len(a_copy)
 
     for i in range(1, n):
-        key = a[i]
+        key = a_copy[i]
 
-        # inserting a[i] into the sorted subarray
-
+        # Inserting a[i] into the sorted subarray
         j = i - 1
-        while j > 0 and a[j] > key:
-            a[j + 1] = a[j]
+        while j >= 0 and a_copy[j] > key:
+            a_copy[j + 1] = a_copy[j]
             j = j - 1
-        a[j + 1] = key
+        a_copy[j + 1] = key
 
-    return a
+    return a_copy
+
+
+a = [5, 3, 4, 6, 1, 3]
+
+b = insertionsort(a)
+print(a)
+print("-------------------")
+print(b)
