1521959adf974b0cf40781994a206c66aa34513c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-13 03:20:58 +0530

    compoiled success

--- Java/CourseraPrinceton/W1/helloAssignment/RandomWord.java
+++ Java/CourseraPrinceton/W1/helloAssignment/RandomWord.java
@@ -7,5 +7,24 @@ public class RandomWord{
 		// takes input from standard input and prints 
 		// one of those words uniformly at random. 
 		// Use Knuth's methodd 
+		// RESERVOIR SAMPLING
+
+		int count = 0 ;
+		String champion = null ;
+		String input = null ;
+
+		// initializing the loop to take inputs 
+		while(!StdIn.isEmpty()){
+			input = StdIn.readString() ; 
+			count++ ; 
+
+			// condition to assign the champion word to get selected 
+			if(StdRandom.random() <  1.0 / count ){
+				champion = input ; 
+			}
+		}
+
+		StdOut.println(champion); 
+
 	}
 }
