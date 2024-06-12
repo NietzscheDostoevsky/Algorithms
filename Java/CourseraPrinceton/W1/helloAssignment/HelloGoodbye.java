95d9f4514fe6edd948bf39f6fe9d7f7ceba14d42
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-12 22:27:32 +0530

    second assignement done

---
+++ Java/CourseraPrinceton/W1/helloAssignment/HelloGoodbye.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class HelloGoodbye{
+
+	public static void main(String[] args) {
+		String firstName = args[0]; 
+		String secondName = args[1] ; 
+
+		StdOut.printf("Hello %s and %s.\n", firstName, secondName);
+		StdOut.printf("Goodbye %s and %s.\n", secondName, firstName); 
+	}
+}
