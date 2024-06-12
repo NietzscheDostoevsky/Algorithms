c89892b1972fc732647bb162f5838c6511f73f53
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-13 03:40:31 +0530

    reformatted docs for styling

--- Java/CourseraPrinceton/W1/helloAssignment/HelloGoodbye.java
+++ Java/CourseraPrinceton/W1/helloAssignment/HelloGoodbye.java
@@ -2,13 +2,13 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class HelloGoodbye{
+public class HelloGoodbye {
 
 	public static void main(String[] args) {
-		String firstName = args[0]; 
-		String secondName = args[1] ; 
+		String firstName = args[0];
+		String secondName = args[1];
 
 		StdOut.printf("Hello %s and %s.\n", firstName, secondName);
-		StdOut.printf("Goodbye %s and %s.\n", secondName, firstName); 
+		StdOut.printf("Goodbye %s and %s.\n", secondName, firstName);
 	}
 }
--- Java/CourseraPrinceton/W1/helloAssignment/RandomWord.java
+++ Java/CourseraPrinceton/W1/helloAssignment/RandomWord.java
@@ -2,29 +2,29 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-public class RandomWord{
+public class RandomWord {
 	public static void main(String[] args) {
-		// takes input from standard input and prints 
-		// one of those words uniformly at random. 
-		// Use Knuth's methodd 
+		// takes input from standard input and prints
+		// one of those words uniformly at random.
+		// Use Knuth's methodd
 		// RESERVOIR SAMPLING
 
-		int count = 0 ;
-		String champion = null ;
-		String input = null ;
+		int count = 0;
+		String champion = null;
+		String input = null;
 
-		// initializing the loop to take inputs 
-		while(!StdIn.isEmpty()){
-			input = StdIn.readString() ; 
-			count++ ; 
+		// initializing the loop to take inputs
+		while (!StdIn.isEmpty()) {
+			input = StdIn.readString();
+			count++;
 
-			// condition to assign the champion word to get selected 
-			if(StdRandom.random() <  1.0 / count ){
-				champion = input ; 
+			// condition to assign the champion word to get selected
+			if (StdRandom.random() < 1.0 / count) {
+				champion = input;
 			}
 		}
 
-		StdOut.println(champion); 
+		StdOut.println(champion);
 
 	}
 }
