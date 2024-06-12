5cdcc335ec059caf14bc198f85614517dd3b363f
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-13 00:01:42 +0530

    Update RandomWord.java

---
+++ Java/CourseraPrinceton/W1/helloAssignment/RandomWord.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord{
+	public static void main(String[] args) {
+		// takes input from standard input and prints 
+		// one of those words uniformly at random. 
+		// Use Knuth's methodd 
+	}
+}
