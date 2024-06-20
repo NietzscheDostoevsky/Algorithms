724d28ef2920728abdfbd39268393b781471d925
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-20 16:21:22 +0530

    Create Flips.java

---
+++ Java/CourseraPrinceton/Chapter01/Section02/Flips.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Flips {
+	public static void main(String[] args) {
+		
+		int T = Integer.parseInt(args[0]);
+		Counter heads = new Counter("heads");
+	}
+}
