d8b16c0e0794e16f6162780156775cb12aa32f38
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-20 16:59:42 +0530

    Create Rolls.java

---
+++ Java/CourseraPrinceton/Chapter01/Section02/Rolls.java
@@ -0,0 +1,7 @@
+// Simulating rolling of a die.
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Counter;
+
