14abec58a3d7091f010e745ddbf51c7b84a10a4b
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-22 15:59:26 +0530

    Create Accumulator.java

---
+++ Java/CourseraPrinceton/Chapter01/Section02/Accumulator.java
@@ -0,0 +1,16 @@
+// Accumulator that returns the running average of values,
+// Doesn't saves the actual values. 
+
+public class Accumulator {
+
+
+}
+
+// Accumulator test code/driver
+
+class AccumulatorDriver {
+	public static void main(String[] args) {
+		
+		
+	}
+}
