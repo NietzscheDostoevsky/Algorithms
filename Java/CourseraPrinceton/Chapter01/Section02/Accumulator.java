7ebda2bb076eca9555a342b99223a5fd2734260f
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-06-22 16:07:11 +0530

    added accumulator code and deirver code

--- Java/CourseraPrinceton/Chapter01/Section02/Accumulator.java
+++ Java/CourseraPrinceton/Chapter01/Section02/Accumulator.java
@@ -1,8 +1,25 @@
 // Accumulator that returns the running average of values,
 // Doesn't saves the actual values. 
 
+import edu.princeton.cs.algs4.*;
+
 public class Accumulator {
 
+	private double total; 
+	private int N;
+
+	public void addDataValue(double val) {
+		N++;
+		total += val;
+	}
+
+	public double mean() {
+		return total/N;
+	}
+
+	public String toString() {
+		return "Mean (" + N + " values): " + String.format("%7.5f", mean());
+	}
 
 }
 
@@ -10,7 +27,13 @@ public class Accumulator {
 
 class AccumulatorDriver {
 	public static void main(String[] args) {
-		
-		
+		int T = Integer.parseInt(args[0]);
+		Accumulator a = new Accumulator();
+
+		for(int i = 0; i < T; t++) {
+			a.addDataValue(StdRandom.random())
+		}
+		StdOut.println(a);
+
 	}
 }
