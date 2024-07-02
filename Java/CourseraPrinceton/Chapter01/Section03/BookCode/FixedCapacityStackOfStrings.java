0f976e7f23c706c67922be8b5d4d8c73e6f1bd71
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-02 22:24:42 +0530

    added code

--- Java/CourseraPrinceton/Chapter01/Section03/BookCode/FixedCapacityStackOfStrings.java
+++ Java/CourseraPrinceton/Chapter01/Section03/BookCode/FixedCapacityStackOfStrings.java
@@ -5,5 +5,41 @@ public class FixedCapacityStackOfStrings {
 	private String[] a ; // Stack entries 
 	private int N; // Sizer of stack  
 
-	
+	public FixedCapacityStackOfStrings(int cap) {
+		a = new String[cap];
+	}
+
+	public boolean isEmpty() {
+		return N == 0 ;
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public void push(String item) {
+		a[N++] = item;
+	}
+
+	public String pop() {
+		return a[--N];
+	}
+
+}
+
+class FixedCapacityStringDriver {
+	public static void main(String[] args) {
+		
+		FixedCapacityStackOfStrings s; 
+		s = new FixedCapacityStackOfStrings(100);// fixed size stack of sieze 100
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if( !item.equals("-")) {
+				s.push(item);
+			} else if (!s.isEmpty()){
+				 StdOut.print(s.pop() + " ");
+			}
+		}
+		StdOut.println("(" + s.size() + " left on stack)");
+	}
 }
