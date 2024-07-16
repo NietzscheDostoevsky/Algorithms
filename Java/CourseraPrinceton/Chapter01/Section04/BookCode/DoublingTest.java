099c3e3ea1413b507e83413d5b43dbf892879974
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-16 16:27:49 +0530

    Create DoublingTest.java

---
+++ Java/CourseraPrinceton/Chapter01/Section04/BookCode/DoublingTest.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.*;
+
+public class DoublingTest {
+	// Time ThreeSum.count() for N random 6-digit ints. 
+	
+	int MAX = 1000_1000; 
+	int[] a = new int[N];
+	for(int i = 0; i < N; i++)
+		a[i] = StdRandom.uniform(-MAX, MAX);
+	Stopwatch timer 
