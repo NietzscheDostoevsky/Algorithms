448deeb2f29446c49b63aadfe6aa51c38af36cd5
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-20 14:42:29 +0530

    Union Find class

---
+++ Java/CourseraPrinceton/Chapter01/Section05/BookCode/UF.java
@@ -0,0 +1,43 @@
+import edu.princeton.cs.algs4.*;
+
+public class UF {
+	private int[] id; // access to component id (side indexed)
+	private int count; // number of components
+
+	public UF(int N) {
+		// Initialize component id array. 
+		count = N; 
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+		}
+	}
+
+	public int count() {
+		return count; 
+	}
+
+	public boolean connected(int p, int q) {
+		return find(p) == find(q); 
+	}
+
+	public int find(int p) {
+		// implemented later 
+	}
+
+	public staic void union(int p, int q) {
+		// implemented later
+	}
+
+	public static void main(String[] args) {
+		// Solve dynamic connectivity problem on StdIn. 
+		
+		int N = StdIn.readInt(); // Read number of sites. 
+		UF uf = new UF(N); 
+		while (!StdIn.isEmpty()) {
+			int p = StdIn.readInt(); 
+			int q = StdIn.readInt(); // Read pairs to connect. 
+			
+		}
+	}
+}
