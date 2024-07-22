78c5ba091b81c29ed8ff35bb5acd585985c264ba
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-22 22:37:35 +0530

    Union find interface

---
+++ Java/CourseraPrinceton/Chapter01/Section05/BookCode/UFI.java
@@ -0,0 +1,16 @@
+pubic interface UFI {
+    // Initialize N sites with integer names (0 to N-1)
+    int N; 
+
+    // Add connection betweeen p and q
+    void union(int p, int q);
+
+    // component identifier for p (0 to N-1)
+    int find(int p);
+
+    //return true if p and q are in the same component
+    boolean connected(int p, int q);
+
+    //Number of components 
+    int count(); 
+}
