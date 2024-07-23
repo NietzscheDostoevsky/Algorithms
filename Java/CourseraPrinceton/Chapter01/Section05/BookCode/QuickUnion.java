f256f84ebc624d0e6762436958af72423cb23be9
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-23 15:02:11 +0530

    implementd driver method

--- Java/CourseraPrinceton/Chapter01/Section05/BookCode/QuickUnion.java
+++ Java/CourseraPrinceton/Chapter01/Section05/BookCode/QuickUnion.java
@@ -1,3 +1,22 @@
+import edu.princeton.cs.algs4.*;
+
 public class QuickUnion implements UFI {
-    
+
+
+
+    public static void main(String[] args) {
+       // main driver method 
+       int N = StdIn.readInt();
+       QuickUnion qu = new QuickUnion(N);
+
+       while (!StdIn.isEmpty()) {
+           int p = StdIn.readInt();
+           int q = StdIn.readInt();
+
+           if(qu.connected(p, q)) continue;
+           qu.union(p, q);
+           StdOut.println(p + " " + q);
+       }
+        StdOut.println(qu.count() + " components"); 
+    }
 }
