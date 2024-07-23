b5b1901841098ed09525929d626b3a3b53dadeb2
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-23 21:50:30 +0530

    updated code

--- Java/CourseraPrinceton/Chapter01/Section05/BookCode/WeightedQuickUnion.java
+++ Java/CourseraPrinceton/Chapter01/Section05/BookCode/WeightedQuickUnion.java
@@ -4,8 +4,37 @@ import edu.princeton.cd.algs4.*;
 
 public class WeightedQuickUnion implements UFI {
 
+    private int[] id; // parent link (site indexed)
+    private int[] sz; // size of component for roots (site indexed)
+    private int count; // number of components
+    
+    
+    public WeightedQuickUnion(int N) { // constructor
+        count = N; 
+        id = new int[N]; 
+
+        for (int i = 0; i < N; i++) {
+            id[i] = i; 
+        }
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            sz[i] = 1; 
+        }
+    }
+
+    public int count() {
+        return count; 
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q); 
+    }
+
+    private int find(int p) {
+        // follow links to find a root
 
 
+    }
 
     public static void main(String[] args) {
         
