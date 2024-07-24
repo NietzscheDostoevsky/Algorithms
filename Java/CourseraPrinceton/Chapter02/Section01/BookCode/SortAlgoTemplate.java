2d4c284daaf09469a6bda0fd106620c8227c4b09
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-24 21:48:37 +0530

    defined interface which sorting algos should adhere to

--- Java/CourseraPrinceton/Chapter02/Section01/BookCode/SortAlgoTemplate.java
+++ Java/CourseraPrinceton/Chapter02/Section01/BookCode/SortAlgoTemplate.java
@@ -1,13 +1,21 @@
+import edu.princeton.cs.algs4.*; 
+
 public interface SortAlgoTemplate {
 
 	// sorting algo to be implemented 
-	abstract void sort((Comparable[]) a);
+	abstract void sort(Comparable[] a) ; 
 
 	// less method, which compares the key between two comparables 
-	abstract boolean less(Comparable v, Comparable w); 
+	default boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0; 
+	}
 
 	// Exchange method, which exchanges the two objects. 
-	abstract void exch(Comparable[] a, int i, int j); 
+	default void exch(Comparable[] a, int i, int j) {
+		Comparable temp = a[i]; 
+		a[i] = a[j]; 
+		a[j] = temp; 
+	}
 
 	// Method show, which shows the array 
 	default void show(Comparable[] a) {
