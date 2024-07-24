471db4d0f0fe5b97f9116bb10bb026c0ba3c46ca
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-24 15:17:00 +0530

    full code complete

--- Java/CourseraPrinceton/Chapter01/Section05/BookCode/WeightedQuickUnion.java
+++ Java/CourseraPrinceton/Chapter01/Section05/BookCode/WeightedQuickUnion.java
@@ -32,8 +32,29 @@ public class WeightedQuickUnion implements UFI {
 
     private int find(int p) {
         // follow links to find a root
+        while(p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
 
+    public void union(int p, int q) {
+        int i = find(p); 
+        int j = find(q);
+        if (i == j) {
+            // do nothing, they are already connected. 
+            return;
+        }
 
+        // Make smaller root point to larger one. 
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];  
+        }
+        count--; 
     }
 
     public static void main(String[] args) {
