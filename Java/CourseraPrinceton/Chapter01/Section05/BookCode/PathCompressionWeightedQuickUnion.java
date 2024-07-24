81c43d1756c4db5d3f4090ad1c0d986e581e586f
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-24 21:33:01 +0530

    updates union method

--- Java/CourseraPrinceton/Chapter01/Section05/BookCode/PathCompressionWeightedQuickUnion.java
+++ Java/CourseraPrinceton/Chapter01/Section05/BookCode/PathCompressionWeightedQuickUnion.java
@@ -1,61 +1,75 @@
-// implementing weighted quick-union 
+// implementing weighted quick-union , with path compression .
 
-import edu.princeton.cs.algs4.*; 
+import edu.princeton.cs.algs4.*;
 
 public class PathCompressionWeightedQuickUnion implements UFI {
 
     private int[] id; // parent link (site indexed)
     private int[] sz; // size of component for roots (site indexed)
     private int count; // number of components
-    
-    
+
     public PathCompressionWeightedQuickUnion(int N) { // constructor
-        count = N; 
-        id = new int[N]; 
+        count = N;
+        id = new int[N];
 
         for (int i = 0; i < N; i++) {
-            id[i] = i; 
+            id[i] = i;
         }
         sz = new int[N];
         for (int i = 0; i < N; i++) {
-            sz[i] = 1; 
+            sz[i] = 1;
         }
     }
 
     public int count() {
-        return count; 
+        return count;
     }
 
     public boolean connected(int p, int q) {
-        return find(p) == find(q); 
+        return find(p) == find(q);
     }
 
     public int find(int p) {
         // follow links to find a root
         while(p != id[p]) {
+            id[p] = id[id[p]];
             p = id[p];
         }
         return p;
     }
 
     public void union(int p, int q) {
-         // implement later,
-        
+        int i = find(p);
+        int j = find(q);
+        if (i == j) {
+            // do nothing, they are already connected.
+            return;
+        }
+
+        // Make smaller root point to larger one.
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        count--;
     }
 
     public static void main(String[] args) {
-        
-        int N = StdIn.readInt(); 
-        PathCompressionWeightedQuickUnion pcwqf = new PathCompressionWeightedQuickUnion(N); 
+
+        int N = StdIn.readInt();
+        PathCompressionWeightedQuickUnion pcwqf = new PathCompressionWeightedQuickUnion(N);
         Stopwatch timer = new Stopwatch();
         while (!StdIn.isEmpty()) {
-            int p = StdIn.readInt(); 
-            int q = StdIn.readInt(); 
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
             if (pcwqf.connected(p, q)) {
                 continue;
             }
             wqf.union(p, q);
-            StdOut.println(q + " " + q); 
+            StdOut.println(q + " " + q);
         }
         double time = timer.elapsedTime();
         StdOut.println(pcwqf.count() + " components");
