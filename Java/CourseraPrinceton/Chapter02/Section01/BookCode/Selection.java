53817b67c5534edfcddd0010e255837f58971b84
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-25 12:26:03 +0530

    selection sort

---
+++ Java/CourseraPrinceton/Chapter02/Section01/BookCode/Selection.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.*; 
+
+public class Selection implements SortAlgoTemplate {
+
+	@Override
+	public static void sort(Comparable[] a) {
+		// sort a[] into increasin order. 
+		int N = a.length;  // array length 
+		for (int i = 0; i < N; i++) {
+			// this loop iterates through the array
+			// Exchange a[i] with the smallest entry in a[i + 1 ... N]
+			int min = i; // index of minimal entry. 
+			for (int j = i+1; j < N; j++) {
+				// this loop compares the element to find the min value in the entire 
+				// array from i onwards.
+				// finds the smallest element's index, and sets it to min.  
+				if (less(a[j], a[min])) { // less returns true if a[j] < a[min]
+					min = j;
+				}				
+			}
+			// after finding the smallest index, exchange the current object 
+			// 	selected by the outer loop to the smallest object found
+			// 	found by the inner loop. 
+			exch(a, i, min); 
+		}
+	}
+
+	
+}
+
