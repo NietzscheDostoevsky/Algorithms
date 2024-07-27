ccf167715067772edcc46c63d03f45814789bcd2
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-27 23:42:41 +0530

    updated to supress compiler warning

--- Java/CourseraPrinceton/Chapter02/Section01/BookCode/Insertion.java
+++ Java/CourseraPrinceton/Chapter02/Section01/BookCode/Insertion.java
@@ -1,49 +1,51 @@
-import edu.princeton.cs.algs4.*; 
-// removed comments to make code shorter. 
-// for complete implementation with comments, see 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+// removed comments to make code shorter.
+// for complete implementation with comments, see
 // selection.java
+@SuppressWarnings({ "rawtypes", "deprecation", "unchecked" })
+public class Insertion {
 
-public class Insertion  {
 
-	
-	public static void sort(Comparable[] a) {
-		// sort a[] in ascending order. 
-		int N = a.length;
-		for (int i = 0; i < N; i++) {
-			// Insert a[i] among a[i-1], a[i-2], a[i-3]... 
-			for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
-				exch(a, j, j-1);
-			}
-		}	
-	}
+    public static void sort(Comparable[] a) {
+        // sort a[] in ascending order.
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            // Insert a[i] among a[i-1], a[i-2], a[i-3]...
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
 
-	private static boolean less (Comparable v, Comparable w) {
-		return v.compareTo(w) < 0; 
-	}
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
 
-	private static void exch (Comparable[] a, int i, int j) {
-		Comparable temp = a[i];
-		a[i] = a[j];
-		a[j] = temp; 
-	}
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
-	private static void show(Comparable[] a) {
-		for (int i = 0; i < a.length; i++)
-			StdOut.print(a[i] + " ");
-		StdOut.println();
-	}
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
 
-	public static boolean isSorted(Comparable[] a) {
-		for (int i = 1; i < a.length; i++) 
-			if (less(a[i], a[i-1])) return false; 
-		return true; 
-	}
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
 
-	public static void main(String[] args) {
-		String[] a = In.readStrings();
-		sort(a); 
-		assert isSorted(a); 
-		show(a);
-	}
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
 }
 
--- Java/CourseraPrinceton/Chapter02/Section01/BookCode/Shell.java
+++ Java/CourseraPrinceton/Chapter02/Section01/BookCode/Shell.java
@@ -1,6 +1,7 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
-@SuppressWarnings({"rawtypes", "deprecation", "unchecked"})
+@SuppressWarnings({ "rawtypes", "deprecation", "unchecked" })
 public class Shell {
     // Sort a[] into increasing order, using ShellSort algorithm
     @SuppressWarnings("rawtypes")
@@ -15,39 +16,39 @@ public class Shell {
             for (int i = h; i < N; i++) {
                 // Insert a[i] among a[i-h], a[i-2*h], a[i-3*h]...
                 for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
-                    exch(a, j, j-h);
+                    exch(a, j, j - h);
                 }
             }
             h = h - 3;
         }
     }
-   
-    private static boolean less (Comparable v, Comparable w) {
-		return v.compareTo(w) < 0; 
-	}
 
-	private static void exch (Comparable[] a, int i, int j) {
-		Comparable temp = a[i];
-		a[i] = a[j];
-		a[j] = temp; 
-	}
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
 
-	private static void show(Comparable[] a) {
-		for (int i = 0; i < a.length; i++)
-			StdOut.print(a[i] + " ");
-		StdOut.println();
-	}
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
-	public static boolean isSorted(Comparable[] a) {
-		for (int i = 1; i < a.length; i++) 
-			if (less(a[i], a[i-1])) return false; 
-		return true; 
-	}
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
 
-	public static void main(String[] args) {
-		String[] a = In.readStrings();
-		sort(a); 
-		assert isSorted(a); 
-		show(a);
-	}
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
 }
