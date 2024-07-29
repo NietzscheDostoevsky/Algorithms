0905c30b5a371a3fdba159362eb5c8de3fe8320e
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-07-29 11:43:48 +0530

    addded merging cod ewith documentatino

--- Java/CourseraPrinceton/Chapter02/Section02/BookCode/Merge.java
+++ Java/CourseraPrinceton/Chapter02/Section02/BookCode/Merge.java
@@ -7,6 +7,35 @@
 package Section02.BookCode;
 
 public class Merge {
-    
+
+    // Merge a[lo...mid] with a[mid+1...hi]
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+
+        int i = lo; // starting index of first array
+        int j = mid + 1; // starting index of seconds array
+
+        // copying the array into an auxiliary array
+        for (int k = lo; k <= hi; k++)
+            aux[k] = a[k];
+
+        // Merging back the two Disjoint Ordered Arrays
+        for (int k = lo; k <= hi; k++) { // outer loop to iterate through the array
+            if (i > mid) {
+                a[k] = aux[j++]; // left half exhausted, take from the right
+            }
+            else if (j > hi) {
+                a[k] = aux[i++]; // right half exhausted, take from the left
+            }
+            else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++]; // current key on right less than current key on left,
+                // take from the right
+            }
+            else {
+                a[k]
+                        = aux[i++]; // current key on the right greater than or equal to current key on left
+                // take from the left.
+            }
+        }
+    }
 
 }
