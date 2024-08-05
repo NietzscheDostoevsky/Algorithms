b811b8192172ea838773b78e0f8bc8f64b9c7450
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-05 18:11:58 +0530

    implementation

--- Java/CourseraPrinceton/Chapter02/Section04/BookCode/MaxPQ.java
+++ Java/CourseraPrinceton/Chapter02/Section04/BookCode/MaxPQ.java
@@ -1,13 +1,36 @@
-/* *****************************************************************************
- *  Name:              Ada Lovelace
- *  Coursera User ID:  123456
- *  Last modified:     October 16, 1842
- **************************************************************************** */
+/*
+* Created By : Saurabh Singh
+* Source: Algorithms, by Sedgewick
+ */
 
-package Section04.BookCode;
+public class MaxPQ<Key extends Comparable<Key>> implements MaxPQInterface {
+    private Key[] pq; // heap-ordered complete binary tree
+    private int N = 0; // in pq[1...N] with pq[0] unused
 
-public class MaxPQ {
-    public static void main(String[] args) {
-
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
     }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public Key delMax() {
+        Key max = pq[1]; // Retrieve max key from top.
+        exch(1, N--); // Exchange eith last item.
+        pq[N+1] = null; // Avoid loitering
+        sing(1); // Restore heap property
+        return max;
+    }
+
+
 }
