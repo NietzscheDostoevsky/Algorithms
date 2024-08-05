ebcdf46d2d90a964b9ef8b8c3bdaf0008a299103
Saurabh Singh <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-05 17:30:24 +0530

    create maxPQ

---
+++ Java/CourseraPrinceton/Chapter02/Section04/BookCode/MaxPQ.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package Section04.BookCode;
+
+public class MaxPQ {
+    public static void main(String[] args) {
+
+    }
+}
