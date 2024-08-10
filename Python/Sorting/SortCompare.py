9c3b6ac9bc97683805298bd16ed81bb095cb2acc
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-10 15:30:03 +0530

    Update SortCompare.py

---
+++ Python/Sorting/SortCompare.py
@@ -0,0 +1,11 @@
+import random 
+from Stopwatch import Stopwatch
+
+#from Bubble Selection import Selection
+
+def time_algo(algo, arr):
+    timer = Stopwatch(); 
+    if algo == "Insertion":
+        Insertion.sort(arr)
+    
+# to be completed after sorting implementations 
