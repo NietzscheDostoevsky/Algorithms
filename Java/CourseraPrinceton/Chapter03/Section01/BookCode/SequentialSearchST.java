2a9a5c8ed38069022e2e841368ae04734530741a
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-14 15:08:09 +0530

    Update SequentialSearchST.java

--- Java/CourseraPrinceton/Chapter03/Section01/BookCode/SequentialSearchST.java
+++ Java/CourseraPrinceton/Chapter03/Section01/BookCode/SequentialSearchST.java
@@ -1,4 +1,38 @@
-public class SequentialSearchST<Key, Value> {
-    
+/*
+ * A simple implementation of Symbol Table using 
+ *      Unordered list. 
+ */
 
+public class SequentialSearchST<Key, Value> {
+    private Node first; // first node of the linked list. 
+    
+    // Implementation of Node for Linked List
+    private class Node {
+        Key key; 
+        Value val;
+        Node next; 
+        public Node(Key key, Value val, Node next) {
+            this.key = key; 
+            this.val = val; 
+            this.next = next; 
+        }
+    }
+
+    // Search for key, return associated value. 
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) 
+            if (key.equals(x.key))
+                return x.val; // search hit
+        return null; // search miss. 
+    }
+
+    // Search for key, Update value if found, grow table if new. 
+    public void put(Key key, Value val) 
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;  // search hits, update value .
+                return;
+            }
+        first = new Node(key, val, first); // search miss, add new node
+    }
 }
