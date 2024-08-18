f5b54e3b153d9123d99d66d2838a5e81e9aa8653
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-18 22:43:22 +0530

    private keys method to enque

--- Java/CourseraPrinceton/Chapter03/Section02/BookCode/BST.java
+++ Java/CourseraPrinceton/Chapter03/Section02/BookCode/BST.java
@@ -363,7 +363,15 @@ public class BST<Key extends Comparable<Key>, Value> {
         return queue;
     }
 
-
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
+    }
+    
 }
 
 
