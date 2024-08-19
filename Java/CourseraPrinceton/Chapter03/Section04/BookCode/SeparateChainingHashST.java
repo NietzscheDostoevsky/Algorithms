80dffab6e37d7e1fbc2345b8dc5524fdde721567
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 21:51:35 +0530

    privatge fileds

--- Java/CourseraPrinceton/Chapter03/Section04/BookCode/SeparateChainingHashST.java
+++ Java/CourseraPrinceton/Chapter03/Section04/BookCode/SeparateChainingHashST.java
@@ -1,2 +1,13 @@
-public class SeparateChainingHashST {
+// Complete implementation of Symbol Table using Separate Chaining Hash Table
+// The key type MUST override equals() and hashcode() methods.
+
+import edu.princeton.cs.algs4.SequentialSearchST;
+
+public class SeparateChainingHashST<Key, Value> {
+    private static final int INIT_CAPACITY = 4;
+
+    private int n;                               // number of key-value pairs
+    private int m;                               // hash table size
+    private SequentialSearchST<Key, Value>[] st; // array of linked-list symbol tables
+    
 }
