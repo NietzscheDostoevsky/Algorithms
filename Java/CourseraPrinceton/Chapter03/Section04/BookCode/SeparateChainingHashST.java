3b76167f25e4167a82d19bdb5143c569c4ebb1e8
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 22:16:15 +0530

    initializing the arrays

--- Java/CourseraPrinceton/Chapter03/Section04/BookCode/SeparateChainingHashST.java
+++ Java/CourseraPrinceton/Chapter03/Section04/BookCode/SeparateChainingHashST.java
@@ -3,11 +3,32 @@
 
 import edu.princeton.cs.algs4.SequentialSearchST;
 
+@SuppressWarnings("unchecked")
 public class SeparateChainingHashST<Key, Value> {
     private static final int INIT_CAPACITY = 4;
 
     private int n;                               // number of key-value pairs
     private int m;                               // hash table size
     private SequentialSearchST<Key, Value>[] st; // array of linked-list symbol tables
-    
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public SeparateChainingHashST() {
+        this(INIT_CAPACITY);
+    }
+
+    /**
+     * Initializes an empty symbol table with {@code m} chains.
+     * @param m the initial number of chains
+     */
+    public SeparateChainingHashST(int m) {
+        this.m = m;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[m];
+        for (int i = 0; i < m; i++)
+            st[i] = new SequentialSearchST<Key, Value>();
+    }
+
+
+
 }
