d012744c0bfcfb840b24d3fbdb2fea7b713986aa
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 09:23:29 +0530

    static vars and node class

--- Java/CourseraPrinceton/Chapter03/Section03/BookCode/RedBlackBST.java
+++ Java/CourseraPrinceton/Chapter03/Section03/BookCode/RedBlackBST.java
@@ -1,2 +1,32 @@
+import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.StdOut;
+
+// Complete implementation of red-black BST
+
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root; // root of the BST
+
+    // BST helper Node data type
+    private class Node {
+        private Key key;        // key
+        private Value val;      // associated data
+        private Node left;      // link to left subtree, parent > child
+        private Node right;     // link to right subtree, parent < child
+        private boolean color;  // color of parent link
+        private int size;       // subtree count;
+
+        public Node(Key key, Value val, boolean color, int size) {
+            this.key = key;
+            this.val = val;
+            this.color = color;
+            this.size = size;
+        }
+    }
+
+    
+}
 
