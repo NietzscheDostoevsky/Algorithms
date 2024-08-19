51d872388fb512f9f76df7a3ecf88c5013aaff3a
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 23:06:28 +0530

    size, isEmpty, contains, hash

--- Java/CourseraPrinceton/Chapter03/Section04/BookCode/LinearProbingHashST.java
+++ Java/CourseraPrinceton/Chapter03/Section04/BookCode/LinearProbingHashST.java
@@ -30,4 +30,49 @@ public class LinearProbingHashST<Key, Value> {
         keys = (Key[]) new Object[m];
         vals = (Value[]) new Object[m];
     }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     *
+     * @return {@code true} if this symbol table is empty;
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Returns true if this symbol table contains the specified key.
+     *
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key};
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    // hash function for keys - returns value between 0 and m-1
+    private int hashTextbook(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    // hash function for keys - returns value between 0 and m-1 (assumes m is a power of 2)
+    // (from Java 7 implementation, protects against poor quality hashCode() implementations)
+    private int hash(Key key) {
+        int h = key.hashCode();
+        h ^= (h >>> 20) ^ (h >>> 12) ^ (h >>> 7) ^ (h >>> 4);
+        return h & (m-1);
+    }
 }
