f733b7525e4fcc57e625e857be1dcfc5a1e65f5e
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 23:00:49 +0530

    constructor

--- Java/CourseraPrinceton/Chapter03/Section04/BookCode/LinearProbingHashST.java
+++ Java/CourseraPrinceton/Chapter03/Section04/BookCode/LinearProbingHashST.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.StdOut;
 // Complete implementation of Symbol Table using Linear Probing Hash Table
 // The key type MUST override equals() and hashcode() methods.
 
+@SuppressWarnings("unchecked")
 public class LinearProbingHashST<Key, Value> {
 
     private static final int INIT_CAPACITY = 4; // must be a power of 2
@@ -10,4 +11,23 @@ public class LinearProbingHashST<Key, Value> {
     private int m;          // size of linear probing table
     private Key[] keys;     // the keys
     private Value[] vals;   // the values
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public LinearProbingHashST() {
+        this(INIT_CAPACITY);
+    }
+
+    /**
+     * Initializes an empty symbol table with the specified initial capacity.
+     *
+     * @param capacity the initial capacity
+     */
+    public LinearProbingHashST(int capacity) {
+        m = capacity;
+        n = 0;
+        keys = (Key[]) new Object[m];
+        vals = (Value[]) new Object[m];
+    }
 }
