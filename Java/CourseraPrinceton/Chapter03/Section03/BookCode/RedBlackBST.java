11c6a56b23ecd1fd6115a09795c8e00656a5c31c
DietzscheNostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-19 09:43:51 +0530

    Node helper functions

--- Java/CourseraPrinceton/Chapter03/Section03/BookCode/RedBlackBST.java
+++ Java/CourseraPrinceton/Chapter03/Section03/BookCode/RedBlackBST.java
@@ -31,8 +31,40 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
      * Initializes an empty symbol table.
      */
     public RedBlackBST() {
-
+        // no argument constructor
     }
 
+    /*******************************************************************
+     *  Node helper methods
+     ****************************************************/
+    // is node x re; false if x is null?
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    // number of node in subtree rooted at x; 0 if x is null;
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.size;
+    }
+
+    /**
+     * Returns the number of key-value paris in this symbol table.
+     * @return the number of key-value pairs in this symbol table.
+     */
+    public int size() {
+        return size(root);
+    }
+
+    /**
+     * Is this symbol table empty?
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    
 }
 
