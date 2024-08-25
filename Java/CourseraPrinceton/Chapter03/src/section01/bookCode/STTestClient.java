e84179c04ab5b746f916154e3e1d1901b96d2f9a
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-25 12:55:38 +0530

    changed entire project structure from intellj to eclipse
    migrated whole project from intellij to eclipse.

---
+++ Java/CourseraPrinceton/Chapter03/src/section01/bookCode/STTestClient.java
@@ -0,0 +1,19 @@
+package section01.bookCode;
+
+import edu.princeton.cs.algs4.*; 
+
+public class STTestClient {
+    public static void main(String[] args) {
+        ST<String, Integer> st ; 
+        st = new ST<String, Integer>(); 
+
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString(); 
+            st.put(key, i); 
+        }
+
+        for (String s: st.keys()) {
+            StdOut.println(s + " " + st.get(s)); 
+        }
+    }
+}
