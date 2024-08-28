afbf7ac8673fa65e63e33b0f90bb1204edc1c788
NietzscheDostoevsky <96907475+NietzscheDostoevsky@users.noreply.github.com>
2024-08-28 13:31:10 +0530

    Graph constructor from another graph

--- Java/CourseraPrinceton/Chapter04/src/section01/bookCode/Graph.java
+++ Java/CourseraPrinceton/Chapter04/src/section01/bookCode/Graph.java
@@ -2,8 +2,11 @@ package section01.bookCode;
 
 import java.util.NoSuchElementException;
 
+import javax.swing.event.AncestorEvent;
+
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 @SuppressWarnings("unchecked")
@@ -11,9 +14,13 @@ public class Graph {
 	
 	private static final String NEWLINE = System.getProperty("line.separator");
 	
-	private final int V;
-	private int E; 
-	private Bag<Integer>[] adj;
+	// Total vertices
+	private final int V; // No of vertex's can't be changed later, once graph is created.
+	// Total edges. 
+	private int E; // more edges can be added later for given vertices. 
+	
+	// Adjacency list of
+	private Bag<Integer>[] adj; // Bag object to store list of vertices adjacent to a vertex.
 	
 	/**
 	 * Initializes an empty graph with {@code V} vertices and 0 edges. 
@@ -63,6 +70,35 @@ public class Graph {
 			}
 		} catch (NoSuchElementException e) {
 			throw new IllegalArgumentException("invalid input format in Graph constructor", e);
+		}
+	}
+	
+	/*
+	 * Initializes a new graph that is deep copy of {@code G} is {@code null}.
+	 * 
+	 * @param IllegalArgumentExceptino if {@code G} is {@code null}
+	 */
+	public Graph(Graph G) {
+		this.V = G.V();
+		this.E = G.E();
+		if (V < 0) throw new IllegalArgumentException("Number of vertices must be non-negative");
+		
+		// Initialize adjacency lists. 
+		adj = (Bag<Integer>[]) new Bag[V];
+		
+		//Add a bag object at each index for V
+		for (int v = 0; v < V; v++)
+			adj[v]= new Bag<Integer>();
+		
+		//Copy the actual data. 
+		for (int v = 0; v < G.V(); v++) {
+			// reverse so that the adjacency list is in same order as original. 
+			// Reverse is necessary here because the Bag implementation is LIFO. 
+			Stack<Integer> reverse = new Stack<Integer>();
+			for (int w : G.adj[v]) // for each bag at index i in G.adj[i]
+				reverse.push(w);
+			for (int w : reverse) // add each vertex to the bag at index i in new Graph
+				adj[v].add(w); 
 		}
 	}
 	
